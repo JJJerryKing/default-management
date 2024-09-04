83d27c3e39ad935ed7a8ace7d3a04bd7080751cf
JJJerryKing <914000903@qq.com>
2024-09-04 09:39:59 +0800

    update

--- mysql/create_table.py
+++ mysql/create_table.py
@@ -1,17 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+'''import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 import sys
-sys.path.append(current_dir)
+sys.path.append(current_dir)'''
 from config import Config
 from models import db
  
- 
-# 在app.config中设置好连接数据库的信息，
-# 然后使用SQLAlchemy(app)创建一个db对象
-# SQLAlchemy会自动读取app.config中连接数据库的信息
-
 
 def create_app():
     app = Flask(__name__)
